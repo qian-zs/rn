f184fb55f3ef7b476984f8d03a5c45494ced10f1
sheng <sleep_qzs@icloud.com>
2022-07-02 09:09:09 +0800

    navigation.popToTop()

--- App.tsx
+++ App.tsx
@@ -22,6 +22,7 @@ const DetailsScreen: React.FC<{ navigation: any; }> = ({ navigation }) => {
       />
       <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
       <Button title='Go back' onPress={() => navigation.goBack()} />
+      <Button title='Go back to first screen in stack' onPress={() => navigation.popToTop()} />
     </View>
   );
 };
