525004c04eaf6c7749eb79a5e660d04bc03c24a9
sheng <sleep_qzs@icloud.com>
2022-07-02 09:06:51 +0800

    增加2种跳转路由

--- App.tsx
+++ App.tsx
@@ -20,13 +20,15 @@ const DetailsScreen: React.FC<{ navigation: any; }> = ({ navigation }) => {
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
+      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
+      <Button title='Go back' onPress={() => navigation.goBack()} />
     </View>
   );
 };
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
